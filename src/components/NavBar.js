f8c7d23fc8eb112a49d24b4cc310334b8bec6b41
Your Name <your.email@example.com>
2024-02-02 00:11:37 +0800

    up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,20 @@
-import "../styles/NavBar.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/NavBar.css";
 
 export default function NavBar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("");
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    toggleNav();
+  };
+
   return (
     <header>
       <nav className="navbar">
@@ -10,24 +23,60 @@ export default function NavBar() {
           alt="Your Logo"
           className="logo"
         />
-        <div className="nav-links">
+        <div className={`nav-links ${isNavOpen ? "show" : ""}`}>
           <ul>
             <li>
-              <Link to="/">HOME</Link>
+              <Link
+                to="/"
+                onClick={() => handleLinkClick("/")}
+                className={activeLink === "/" ? "active" : ""}
+              >
+                HOME
+              </Link>
             </li>
             <li>
-              <Link to="/Projects">PROJECTS</Link>
+              <Link
+                to="/Projects"
+                onClick={() => handleLinkClick("/Projects")}
+                className={activeLink === "/Projects" ? "active" : ""}
+              >
+                PROJECTS
+              </Link>
             </li>
             <li>
-              <Link to="/Services">SERVICES</Link>
+              <Link
+                to="/Services"
+                onClick={() => handleLinkClick("/Services")}
+                className={activeLink === "/Services" ? "active" : ""}
+              >
+                SERVICES
+              </Link>
             </li>
             <li>
-              <Link to="/About">ABOUT US</Link>
+              <Link
+                to="/About"
+                onClick={() => handleLinkClick("/About")}
+                className={activeLink === "/About" ? "active" : ""}
+              >
+                ABOUT US
+              </Link>
             </li>
             <li>
-              <Link to="/Inquiries">INQUIRIES</Link>
+              <Link
+                to="/Inquiries"
+                onClick={() => handleLinkClick("/Inquiries")}
+                className={activeLink === "/Inquiries" ? "active" : ""}
+              >
+                INQUIRIES
+              </Link>
             </li>
           </ul>
+        </div>
+        <div
+          className={`icon ${isNavOpen ? "active" : ""}`}
+          onClick={toggleNav}
+        >
+          &#9776; {/* Burger icon */}
         </div>
       </nav>
     </header>
